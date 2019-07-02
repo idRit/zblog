861c0495f4eb41540abcdc6c3900a3eb146f6260
Ritwik Mukherjee <mritwik369@gmail.com>
2019-07-02 17:02:58 +0530

    yeet

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,16 @@
 </script>
 
 <style>
-  .bgimg1 {
+  /* .caption span.border {
+    background-color: #111;
+    color: #fff;
+    padding: 18px;
+    font-size: 25px;
+    letter-spacing: 10px;
+  } */
+
+  .bgimg1,
+  .bgimg2 {
     position: relative;
     opacity: 0.65;
     background-attachment: fixed;
@@ -28,6 +37,11 @@
     min-height: 100%;
   }
 
+  .bgimg2 {
+    background-image: url("https://scontent-lga3-1.cdninstagram.com/vp/7a2e4ad2db9f83ffa243edf413692bc5/5DC28F92/t51.2885-15/e35/41339795_471036513381082_6857434592559759360_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com");
+    min-height: 100%;
+  }
+
   .caption {
     position: absolute;
     left: 0;
@@ -37,39 +51,58 @@
     display: flex;
   }
 
-  .caption span.border {
-    background-color: #111;
-    color: #fff;
-    padding: 18px;
-    font-size: 25px;
-    letter-spacing: 10px;
+  .footerarea {
+    display: flex;
+    text-align: center;
+    display: block;
+  }
+
+  .footeritem {
+    padding: 20px;
+    box-sizing: border-box;
+    margin-right: 5%;
+    margin-top: 5%;
   }
 
   .zlogo {
     margin: auto;
   }
 
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+  }
+
   .card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-    width: 40%;
+    width: 200px;
+    margin: 10px;
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
+    color: aliceblue;
+  }
+  .card img {
+    max-width: 100%;
+  }
+  .card .text {
+    padding: 0 20px 20px;
+  }
+  .card .text > button {
+    background: gray;
+    border: 0;
+    color: white;
+    padding: 10px;
+    width: 100%;
+  }
+  .card:nth-child(1) {
+    width: 420px;
   }
 
-  .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
-
-  .container {
-    padding: 2px 16px;
-  }
-  /* Turn off parallax scrolling for tablets and phones */
   @media only screen and (max-width: 480px) {
     .bgimg1 {
       background-image: url("https://drive.google.com/uc?export=download&id=1_JJZeI8seclFvmZKRodUi9PKflnfMXGM");
-      background-attachment: scroll;
     }
   }
-  
 </style>
 
 <div class="bgimg1">
@@ -80,13 +113,12 @@
       height="75%"
       src="https://drive.google.com/uc?export=download&id=1OQD-t5-CHCmGp1IqqVAAfVGLoIG1eouo"
       alt="" />
-
     <!-- <span class="border"> {name} </span> -->
   </div>
 </div>
 
 <div
-  style="color: #777;background-color:white;text-align:center;padding:50px
+  style="color: white;background-color:black;text-align:center;padding:50px
   80px;text-align: justify;">
   <h3 style="text-align:center;">About Me</h3>
   {#await aboutMePromise}
@@ -100,18 +132,52 @@
   {/await}
 </div>
 
-<!-- <div class="bgimg1">
-  <div class="w3-card-4" style="width:50%; background-color: white">
-    <img src="https://scontent-lga3-1.cdninstagram.com/vp/1387ec8e0ebbd2a40b965fbe05e88f2d/5DB0432E/t51.2885-15/e35/51762062_358777364977782_4229431296205314296_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" alt="Alps" style="width:100%">
-    <div class="w3-container w3-center">
-      <p>The Italian / Austrian Alps</p>
-    </div>
+<div class="bgimg2">
+  <h3 style="text-align:center; padding:50px 80px; color: white;">
+    Recent Reads
+  </h3>
+  <div class="cards-container">
+    <article class="card">
+      <img
+        src="https://drive.google.com/uc?export=download&id=1iny3jfjv5lBapozv3Jx614HkQTBUtIpZ"
+        alt="Sample photo" />
+      <div class="text">
+        <h3>Dynamically Procrastinate</h3>
+        <p>
+          Completely synergize resource taxing relationships via premier niche
+          markets.
+        </p>
+        <button>Read more</button>
+      </div>
+    </article>
+    <article class="card">
+      <img
+        src="https://drive.google.com/uc?export=download&id=1iny3jfjv5lBapozv3Jx614HkQTBUtIpZ"
+        alt="Sample photo" />
+      <div class="text">
+        <h3>Dynamically Procrastinate</h3>
+        <p>
+          Completely synergize resource taxing relationships via premier niche
+          markets.
+        </p>
+        <button>Read more</button>
+      </div>
+    </article>
   </div>
-</div> -->
+</div>
 
-<!-- <img
-      class="border"
-      src="https://drive.google.com/uc?export=download&id=1CkLhBhPN7uJ97bsSl4TZDxXxREIMq0cO"
-      width="50%"
-      height="50%"
-      alt="" /> -->
+<footer
+  style="color: white;background-color:black;text-align:center;padding:10px
+  10px;text-align: justify;">
+  <h3 style="text-align:center;">Contact Me</h3>
+  <div class="footerarea">
+    <a class="footeritem" href="mailto:someone@example.com">
+      someone@example.com
+    </a>
+    <a class="footeritem" href="https://www.instagram.com/zeddshaikh/">
+      instagram
+    </a>
+    <a class="footeritem" href="mailto:someone@example.com">reddit</a>
+    <a class="footeritem" href="mailto:someone@example.com">quora</a>
+  </div>
+</footer>
