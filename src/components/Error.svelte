d8e0f5ea14b5de47e98283da99111d5fd2458b1e
Ritwik Mukherjee <mritwik369@gmail.com>
2019-07-04 12:05:32 +0530

    error

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -1,1 +1,85 @@
-<p>404 phek</p>
+<style>
+  .bgimg1 {
+    position: relative;
+    opacity: 0.65;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .bgimg1 {
+    background-image: url("https://drive.google.com/uc?export=download&id=1iny3jfjv5lBapozv3Jx614HkQTBUtIpZ");
+    min-height: 100%;
+  }
+
+  .caption {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    text-align: center;
+    display: flex;
+  }
+
+  .caption span.border {
+    background-color: #111;
+    color: #fff;
+    padding: 18px;
+    font-size: 25px;
+    letter-spacing: 10px;
+    margin: auto;
+  }
+
+  .foot {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+  }
+
+  .footerarea {
+    display: flex;
+    text-align: center;
+    display: block;
+  }
+
+  .footerarea {
+    display: flex;
+    text-align: center;
+    display: block;
+  }
+
+  .footeritem {
+    padding: 20px;
+    box-sizing: border-box;
+    margin-right: 5%;
+    margin-top: 5%;
+  }
+</style>
+
+<div class="bgimg1">
+  <div class="caption">
+    <span class="border">404 Not Found!</span>
+  </div>
+  <footer
+    style="color: white;background-color:black;text-align:center;padding:10px
+    10px;text-align: justify;"
+    class="foot">
+    <h3 style="text-align:center;">Contact Me</h3>
+    <div class="footerarea">
+      <a class="footeritem" href="mailto:zeddshaikh@gmail.com">
+        zeddshaikh@gmail.com
+      </a>
+      <a class="footeritem" href="https://www.instagram.com/zeddshaikh/">
+        instagram
+      </a>
+      <a class="footeritem" href="https://www.twitter.com/zeddshaikh/">
+        twitter
+      </a>
+      <a class="footeritem" href="https://www.reddit.com/user/zeddshaikh">
+        reddit
+      </a>
+    </div>
+  </footer>
+</div>
