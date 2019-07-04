9c90085bd518ca82ca2f1546262910863c41df81
Ritwik Mukherjee <mritwik369@gmail.com>
2019-07-04 11:52:30 +0530

    gfghhj

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
+  res.render(path.join(__dirname + '/public/index.html'))
 });
 
 app.listen(process.env.PORT || 8080, () => {
