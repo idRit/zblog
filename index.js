e3efb82637f690a0ada0809f1e6fb4a226b21b1d
Ritwik Mukherjee <mritwik369@gmail.com>
2019-07-04 11:46:34 +0530

    dgj

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'))
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log('App started');
 });
